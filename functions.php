eac61198123d4fd1d368187e8d24a319cdcc3ce6
Abdullah Al Arif <arif@xpeedstudio.com>
2023-10-20 00:00:39 +0600

    basic bootstrap portfolio theme

---
+++ functions.php
@@ -0,0 +1,92 @@
+<?php 
+
+function add_theme_scripts() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+	wp_enqueue_style( 'montserrat-font', '//fonts.googleapis.com/css?family=Montserrat:400,700' );
+	wp_enqueue_style( 'lato-font', '//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' );
+	wp_enqueue_style( 'main-css', get_template_directory_uri() . '/css/styles.css', array(), '1.1', 'all' );
+
+	
+	
+	wp_enqueue_script( 'main-script', get_template_directory_uri() . '/js/scripts.js', true);
+	// Enqueue my scripts.
+    wp_enqueue_script( 'main-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array(), null, true );
+    wp_enqueue_script( 'sb', 'https://cdn.startbootstrap.com/sb-forms-latest.js', array(), null, true );
+   
+
+	
+
+	
+}
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+function basic_functions(){
+    add_theme_support('title-tag');
+    add_theme_support('custom-background');
+    add_theme_support('post-thumbnails');
+
+    $defaults = array(
+		'height'               => 100,
+		'width'                => 400,
+		'flex-height'          => true,
+		'flex-width'           => true,
+		'header-text'          => array( 'site-title', 'site-description' ),
+		'unlink-homepage-logo' => true, 
+	);
+	add_theme_support( 'custom-logo', $defaults );
+}
+
+add_action('after_setup_theme', 'basic_functions');
+
+function register_my_menus() {
+    register_nav_menus(
+      array(
+        'header-menu' => __( 'Header Menu' ),
+        'extra-menu' => __( 'Extra Menu' )
+       )
+     );
+   }
+   add_action( 'init', 'register_my_menus' );
+
+   function amader_custom_post_type() {
+	register_post_type('portfolio-post',
+		array(
+			'labels'      => array(
+				'name'          => __( 'PortFolio', 'textdomain' ),
+				'singular_name' => __( 'PortFolio', 'textdomain' ),
+			),
+			'public'      => true,
+			'has_archive' => true,
+			'supports' => array('title','thumbnail', 'editor'),
+			'rewrite'     => array( 'slug' => 'portfolio' ), // my custom slug
+		)
+	);
+}
+add_action('init', 'amader_custom_post_type');
+
+
+function add_specific_menu_location_atts( $atts, $item, $args ) {
+    // check if the item is in the primary menu
+    if( $args->theme_location == 'header-menu' ) {
+      // add the desired attributes:
+	 
+	   $atts['class'] = 'nav-link py-3 px-0 px-lg-3 rounded';
+      
+    }
+    return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'add_specific_menu_location_atts', 10, 3 );
+
+function add_custom_css_class_to_menu_item($classes, $item, $args, $depth) {
+    // Check if the menu location matches the one you want to target
+    if ($args->theme_location === 'header-menu') {
+        // Add your custom CSS class to the existing classes
+        $classes[] = 'nav-item mx-0 mx-lg-1';
+    }
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'add_custom_css_class_to_menu_item', 10, 4);
+
+
+//https://teamtreehouse.com/community/open-posts-in-popup-window 
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        
+<?php wp_head(); ?>
+    </head>
+    <body id="page-top" <?php body_class(); ?>>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+
+                <?php 
+                    wp_nav_menu(
+                        array(
+                          'theme_location' => 'header-menu',
+                          'menu_class' => 'navbar-nav ms-auto',
+                          'container' => 'ul',
+                        )
+                      );
+                ?>
+                    <!-- <ul class="navbar-nav ms-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
+                    </ul> -->
+                </div>
+            </div>
+        </nav>
