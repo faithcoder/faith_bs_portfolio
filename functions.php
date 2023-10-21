51af4ef30501cb876b3bfecdaed2bc4102d64e24
Abdullah Al Arif <faithcoder1@gmail.com>
2023-10-21 23:47:08 +0600

    Dynamic About Section

--- functions.php
+++ functions.php
@@ -73,38 +73,46 @@ add_filter('nav_menu_css_class', 'add_custom_css_class_to_menu_item', 10, 4);
 
 function hello_customizer($wp_customize){
 
+	
+	//Theme Settings Panel
 	$wp_customize->add_panel( 'menus', array(
-	'title' => __( 'Theme Settings' ),
-	'description' => "hello", // Include html tags such as <p>.
-	'priority' => 160, // Mixed with top-level-section hierarchy.
+		'title' => __( 'Theme Settings' ),
+		'description' => "hello", // Include html tags such as <p>.
+		'priority' => 160, // Mixed with top-level-section hierarchy.
 	) );
 	
 	
 	$wp_customize->add_section('banner_section', array(
-	'title' => __('Banner Section', 'b255'),
-	'priority' => 10,
-	'panel' => 'menus'
+		'title' => __('Banner Section', 'b255'),
+		'priority' => 10,
+		'panel' => 'menus'
 	));
+
 	$wp_customize->add_setting('banner_heading', array(
-	'default' => __('Hello World'),
-	'transport' => 'postMessage',
+		'default' => __('Hello World'),
+		'transport' => 'postMessage',
 	));
-	$wp_customize->add_control('banner-heading-control',array(
-	'label' => __('Write Heading', 'b255'),
-	'type' => 'text',
-	'settings' => 'banner_heading',
-	'section' => 'banner_section',
+	
+	$wp_customize->add_control('banner-heading-control',
+	array(
+		'label' => __('Write Heading', 'b255'),
+		'type' => 'text',
+		'settings' => 'banner_heading',
+		'section' => 'banner_section',
 	));
-	$wp_customize->add_setting('banner_subheading', array(
-	'default' => __('amader text area'),
+
+	$wp_customize->add_setting('banner_subheading', 
+	array(
+		'default' => __('amader text area'),
 	));
 	
 			
-	$wp_customize->add_control('banner-text-control',array(
-	'label' => __('Write Sub Heading', 'b255'),
-	'type' => 'textarea',
-	'settings' => 'banner_subheading',
-	'section' => 'banner_section',
+	$wp_customize->add_control('banner-text-control',
+	array(
+		'label' => __('Write Sub Heading', 'b255'),
+		'type' => 'textarea',
+		'settings' => 'banner_subheading',
+		'section' => 'banner_section',
 	));
 
 	$wp_customize->add_setting('banner_image', array(
@@ -119,6 +127,131 @@ function hello_customizer($wp_customize){
 			'context' => 'your_setting_context'
 			)
 		));
+
+	//ABOUT SECTION
+	$wp_customize->add_section('about_section', array(
+		'title' => __('About Section', 'b255'),
+		'priority' => 10,
+		'panel' => 'menus'
+	));
+	//header controller
+	$wp_customize->add_setting('about_heading', array(
+		'default' => __('About Us'),
+		'transport' => 'postMessage',
+	));
+	
+	$wp_customize->add_control('about-heading-control',
+	array(
+		'label' => __('About Heading', 'b255'),
+		'type' => 'text',
+		'settings' => 'about_heading',
+		'section' => 'about_section',
+	));
+
+
+	//left side text controller
+	$wp_customize->add_setting('left_side_text', array(
+		'default' => __('Freelancer is a free bootstrap theme created by Start Bootstrap.'),
+		'transport' => 'postMessage',
+	));
+	
+	$wp_customize->add_control('left_side_text_control',
+	array(
+		'label' => __('About Left Side Text', 'b255'),
+		'type' => 'textarea',
+		'settings' => 'left_side_text',
+		'section' => 'about_section',
+	));
+
+	//right side text controller
+	$wp_customize->add_setting('right_side_text', array(
+		'default' => __('Freelancer is a free bootstrap theme created by Start Bootstrap.'),
+		'transport' => 'postMessage',
+	));
+	
+	$wp_customize->add_control('right_side_text_control',
+	array(
+		'label' => __('About Right Side Text', 'b255'),
+		'type' => 'textarea',
+		'settings' => 'right_side_text',
+		'section' => 'about_section',
+	));
+
+
+	//About Section Button controller
+	$wp_customize->add_setting('about_button_text', array(
+		'default' => __('Free Download'),
+		'transport' => 'postMessage',
+	));
+	
+	$wp_customize->add_control('about_button_text_control',
+	array(
+		'label' => __('About Button Text', 'b255'),
+		'type' => 'text',
+		'settings' => 'about_button_text',
+		'section' => 'about_section',
+	));
+	//About Button URL controller
+	$wp_customize->add_setting('about_button_url', array(
+		'default' => __('#'),
+		'transport' => 'postMessage',
+	));
+	
+	$wp_customize->add_control('about_button_url_control',
+	array(
+		'label' => __('About Button URL', 'b255'),
+		'type' => 'url',
+		'settings' => 'about_button_url',
+		'section' => 'about_section',
+	));
+
+	//about button text color control
+	$wp_customize->add_setting('primary_color', array(
+        'default' => '#ff0000', // Default color
+        'sanitize_callback' => 'sanitize_hex_color',
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'primary_color', array(
+        'label' => __('Button Text Color', 'your-theme-text-domain'),
+        'section' => 'about_section',
+    )));
+	//primary_bg_color
+	$wp_customize->add_setting('primary_bg_color', array(
+        'default' => '#ff0000', // Default color
+        'sanitize_callback' => 'sanitize_hex_color',
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'primary_bg_color', array(
+        'label' => __('Button Background Color', 'your-theme-text-domain'),
+        'section' => 'about_section',
+    )));
+	//button_border_color
+	$wp_customize->add_setting('button_border_color', array(
+        'default' => '#ff0000', // Default color
+        'sanitize_callback' => 'sanitize_hex_color',
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'button_border_color', array(
+        'label' => __('Button border Color', 'your-theme-text-domain'),
+        'section' => 'about_section',
+    )));
 }
 
 add_action('customize_register', 'hello_customizer');
+
+
+function theme_custom_css() {
+    $primary_color = get_theme_mod('primary_color', '#666666'); // Get the custom color or use the default if not set.
+    $primary_bg_color = get_theme_mod('primary_bg_color', '#dddddd'); // Get the custom color or use the default if not set.
+    $button_border_color = get_theme_mod('button_border_color', '#e5e5e5'); // Get the custom color or use the default if not set.
+
+    echo '<style type="text/css">
+        /* CSS rules that use the custom color */
+        a.btn.btn-xl {
+            color: ' . esc_attr($primary_color) . ';
+            background-color: ' . esc_attr($primary_bg_color) . ';
+            border: 3px solid' . esc_attr($button_border_color) . ';
+        }
+    </style>';
+}
+add_action('wp_head', 'theme_custom_css');
